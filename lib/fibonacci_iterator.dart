8402d6a1f63d788e2445649908edbf66218b1594
Paul Fryzel <paul.fryzel@gmail.com>
2014-03-10 00:40:36 -0400

    Conform to dart:core Iterator spec
    - return null if the iterator has not yet been moved
    - return null if the iterator has been moved after the last element
    - repeat moveNext() invocations should be safe
    
    via: https://api.dartlang.org/apidocs/channels/dev/#dart-core.Iterator

--- lib/fibonacci_iterator.dart
+++ lib/fibonacci_iterator.dart
@@ -1,14 +1,29 @@
 library fibonacci_iterator;
 
 class FibonacciIterator implements Iterator<int> {
-  int _current = 0;
+  final int _count;
+  bool _initial = true;
+  int _index = 0;
+  int _current = null;
   int _next = 1;
 
+  FibonacciIterator(this._count);
+
   bool moveNext() {
-    int next_next = _current + _next;
-    this._current = _next;
-    this._next = next_next;
-    return true;
+    if (_initial && _current == null) {
+      _initial = false;
+      _current = 0;
+      return true;
+    } else if (_index < _count) {
+      _index++;
+      int next_next = _current + _next;
+      _current = _next;
+      _next = next_next;
+       return true;
+    } else {
+      _current = null;
+      return false;
+    }
   }
 
   int get current => _current;
--- test/fibonacci_iterator_test.dart
+++ test/fibonacci_iterator_test.dart
@@ -5,13 +5,30 @@ import 'package:unittest/unittest.dart';
 import 'package:fibonacci_iterator/fibonacci_iterator.dart';
 
 void main() {
-  test('Fibonacci Iterator', () {
+  group('Fibonacci Iterator', () {
     List<int> check = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144];
-    FibonacciIterator fibonacci = new FibonacciIterator();
+    FibonacciIterator fibonacci = new FibonacciIterator(check.length - 1);
 
-    for (int n in check) {
-      expect(fibonacci.current, n);
-      expect(fibonacci.moveNext(), true);
-    }
+    // via: https://api.dartlang.org/apidocs/channels/dev/#dart-core.Iterator
+
+    test('returns null if the iterator has not yet been moved', () {
+      expect(fibonacci.current, null);
+    });
+
+    test('iterates over the expected range of Fibonacci numbers', () {
+      for (int n in check) {
+        expect(fibonacci.moveNext(), true);
+        expect(fibonacci.current, n);
+      }
+    });
+
+    test('returns false if no element was left', () {
+      expect(fibonacci.moveNext(), false);
+      expect(fibonacci.moveNext(), false); // repeat should be safe
+    });
+
+    test('returns null if the iterator has been moved after the last element', () {
+      expect(fibonacci.current, null);
+    });
   });
 }
